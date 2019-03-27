c00c9c77f4710ea0db7b8b071913f0dcdd17621d
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2019-03-26 17:28:59 -0700

    add code to detect name and validate input for form

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 'use strict';
 
-var startQuiz = function(name){
+var startQuiz = function(event){
+  event.preventDefault();
+  var userName = document.getElementById("username").value;
+
+  if (userName === "") {
+    alert("Please type your name and hit 'Submit' to get started");
+  } else {
+    console.log("User's name is " + userName);
+  }
+  
 
 }
 
