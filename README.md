67ff5fde5208a042d09e4fbc18f826c0bd872ba6
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2019-03-28 08:57:12 -0700

    add number guess game, fix eslint errors upadte readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # about-me
 An about me page
+
+# Update on 3/27/2019
+- added details to me about me page in the different sections
+
+- added a sixth question to the quiz
+- added - seventh question to the quiz
--- app.js
+++ app.js
@@ -1,28 +1,35 @@
 'use strict';
 
+//displays updated score to screen
 var updateScore = function(numCorrect, numQuestions){
-  document.getElementById("num-correct").innerHTML = "<span><strong ></strong></span>" +  numCorrect + " / " + numQuestions + " Correct";
-}
+  document.getElementById('num-correct').innerHTML = '<span><strong ></strong></span>' + numCorrect + ' / ' + numQuestions + ' Correct';
+};
 
+//returns  randon number between 0 and value, num, entered
+var getARandomNumber = function(num) {
+  return Math.floor(Math.random() * num) + 1;
+};
+
+//startQuiz is called in the HTML doc
 var startQuiz = function(event){
   event.preventDefault();
-  var userName = document.getElementById("username").value;
+  var userName = document.getElementById('username').value;
 
-  if (userName === "") {
-    console.log("User entered blank username - try again")
-    alert("Please type your name and hit 'Submit' to get started");
+  if (userName === '') {
+    console.log('User entered blank username - try again');
+    alert('Please type your name and hit "Submit" to get started');
   } else {
-    console.log("User's name is " + userName);
+    console.log('User\'s name is ' + userName);
 
     document.getElementsByTagName('form')[0].style.display = 'none';
-    document.getElementById("questions").style.display = 'block';
-    document.getElementById("display-name").innerText = userName;
+    document.getElementById('questions').style.display = 'block';
+    document.getElementById('display-name').innerText = userName;
 
     //number of correct answers
     var numCorrect = 0;
 
-    //set up questions, answers, and explanations
-    var questions = [
+    //set up questions, answers, and explanations for yes/no section of quiz
+    var yesAndNoQuestions = [
       'I started coding in college.', 
       'I served as an Electrician in the Navy.',
       'I recently learned the php programming language.',
@@ -30,44 +37,49 @@ var startQuiz = function(event){
       'I previously worked as a statistician'
     ];
 
-    var answers = ['no', 'yes', 'no', 'no', 'yes'];
+    var yesAndNoAnswers = ['no', 'yes', 'no', 'no', 'yes'];
 
-    var explanations = ['I started to code in high school.', 
-                        'I served as an Electrician in the Navy in a group called the Seabees.',
-                        'php is actually the laguage that I am least interested in learning, but if I have to, I will.',
-                        'Once upon a time, my favorite color was blue, but it is now orange',
-                        'I worked as a Statistican for a year in Trinidad and Tobago, before moving to Grenada.'
-                      ];
+    var yesAndNoExplanations = ['I started to code in high school.', 
+      'I served as an Electrician in the Navy in a group called the Seabees.',
+      'php is actually the laguage that I am least interested in learning, but if I have to, I will.',
+      'Once upon a time, my favorite color was blue, but it is now orange',
+      'I worked as a Statistican for a year in Trinidad and Tobago, before moving to Grenada.'
+    ];
 
-    var correctResponseMessage = "Yes, you are correct! ";
-    var incorrectResponseMessage = "Sorry, that is not the correct response. "
+    //set up questions, answers, and explanations for Guess a number section of quiz
+    var guessANumberQuestions = ['I am thinking of a number between 1 and 10, can you guess what it is?'];
+    var guessANumberAnswers = [getARandomNumber(10)];
+    var guessANumberExplanations = ['The number that I was thinking about was '];
+  
+    //Generic responses for correct/incorrect
+    var correctResponseMessage = 'Yes, you are correct! ';
+    var incorrectResponseMessage = 'Sorry, that is not the correct response. ';
 
     //Icons to mark questions as correct or incorrect
     var correctResponseIcon = '<span><i class="far fa-check-circle"></i></span>';
     var incorrectResponseIcon = '<span><i class="far fa-times-circle"></i></span>';
 
     //Get array of elements for questions
-    var questionDivs = document.getElementsByClassName("question");
+    var questionDivs = document.getElementsByClassName('question');
     
-    //populate questions on DOM
-    for (var i = 0; i < questions.length; i++) {
+    //populate questions on DOM for Yes and No questions
+    for (var i = 0; i < yesAndNoQuestions.length; i++) {
       // set up imput to match input to question index in array
-      var idName = "quiz-response-" + i;
-      questionDivs[i].innerHTML = "<h3>" + questions[i] + "</h3><input type='text' class='quiz-response' id=" + idName + " placeholder='type yes/no'></input>"; 
+      var idName = 'quiz-response-' + i;
+      questionDivs[i].innerHTML = '<h3>' + yesAndNoQuestions[i] + '</h3><input type="text" class="quiz-response" id=' + idName + ' placeholder="type yes/no"></input>'; 
     }
 
-    updateScore(numCorrect, questions.length);
-    
-    var quizResponses = document.getElementsByClassName("quiz-response");
+    updateScore(numCorrect, yesAndNoQuestions.length + guessANumberQuestions.length + 1);
+    var quizResponses = document.getElementsByClassName('quiz-response');
 
     //add event listeners for input boxes for when enters answer
-    for (var i = 0; i < quizResponses.length; i++) {
-      quizResponses[i].addEventListener('keypress', function (event){
+    for (var j = 0; j < quizResponses.length; j++) {
+      quizResponses[j].addEventListener('keypress', function (event){
         var keyPressed = event.which || event.keyCode;
         
         //check to see if "Enter key is depressed"
         if (keyPressed === 13) {
-          //tests that were used to extract value 
+          //tests that were used to extract value
           // console.log("Enter is pressed, check value in box");
           // console.log(event);
           // console.log("Value entered is " + event.srcElement.value);
@@ -75,50 +87,71 @@ var startQuiz = function(event){
 
           var userResponse = event.srcElement.value.toLowerCase().trim();
           var questionNumber = parseInt(this.id[this.id.length - 1]);
-          console.log("Response entered for question " + (questionNumber + 1));
-          
-          if (userResponse === "yes" || userResponse === "no") {
-            console.log("Valid response entered. User answered " + userResponse + " for question " + (questionNumber + 1));
-            console.log("input box is disabled");
+          console.log('Response entered for question ' + (questionNumber + 1));
+       
+          if (userResponse === 'yes' || userResponse === 'no') {
+            console.log('Valid response entered. User answered ' + userResponse + ' for question ' + (questionNumber + 1));
+            console.log('input box is disabled');
 
-            //
-            document.getElementById("instructions").style.backgroundColor = "white";
-            
+            document.getElementById('instructions').style.backgroundColor = 'white';
+
             //disable field from further input
-            console.log(this)
+            console.log(this);
             this.placeholder = userResponse;
 
 
-            if (userResponse === answers[questionNumber]) {
-              console.log("Correct response entered for question " + (questionNumber + 1));
+            if (userResponse === yesAndNoAnswers[questionNumber]) {
+              console.log('Correct response entered for question ' + (questionNumber + 1));
 
               //display message for notifiction of correct response
-              questionDivs[questionNumber].innerHTML += "    " + correctResponseIcon + "<span class='correct'>" + correctResponseMessage + explanations[questionNumber] + "</span>";
+              questionDivs[questionNumber].innerHTML += '    ' + correctResponseIcon + '<span class="correct">' + correctResponseMessage + yesAndNoExplanations[questionNumber] + '</span>';
               
               //update current score and append to DOM
               numCorrect += 1;
-              updateScore(numCorrect, questions.length);
+              updateScore(numCorrect, yesAndNoQuestions.length + guessANumberQuestions.length + 1);
 
             } else {
-              console.log("i = " + i);
-              console.log("incorrect response entered");
-              console.log(incorrectResponseMessage + explanations[questionNumber]);
+              console.log('j = ' + j);
+              console.log('incorrect response entered');
+              console.log(incorrectResponseMessage + yesAndNoExplanations[questionNumber]);
               
               //display message for notification of incorrect response
-              questionDivs[questionNumber].innerHTML += "    " + incorrectResponseIcon + "<span class='incorrect'>" + incorrectResponseMessage + explanations[questionNumber] + "</span>";
+              questionDivs[questionNumber].innerHTML += '    ' + incorrectResponseIcon + '<span class="incorrect">' + incorrectResponseMessage + yesAndNoExplanations[questionNumber] + '</span>';
             }
-            
-
           } else {
-            console.log("Invalid response entered - try again");
+            console.log('Invalid response entered - try again');
             //reset this input field
-            document.getElementById("instructions").style.backgroundColor = "pink";
-            this.value = "";
+            document.getElementById('instructions').style.backgroundColor = 'pink';
+            this.value = '';
           }
         }
-      })
-    }  
+      });
+    }
+    //TODO refactor below a function for guessing game
+    var userGuess = prompt('You have 3 guesses. ' + guessANumberQuestions[0]);
+
+    while (isNaN(parseInt(userGuess))) {
+      userGuess = prompt('Please enter a NUMBER. ' + guessANumberQuestions[0]);
+    }
+
+    var numGuessesRemaining = 2;
+
+    while (numGuessesRemaining > 0 ) {
+      var answer = guessANumberAnswers[0];
+      if (parseInt(userGuess) === answer){
+        alert (correctResponseMessage);
+        numCorrect += 1;
+        break;
+      } else if (userGuess <= answer) {
+        userGuess = prompt('Too low, guess again. ' + guessANumberQuestions[0]);
+      } else {
+        userGuess = prompt('Too High, guess again. ' + guessANumberQuestions[0]);
+      }
+      numGuessesRemaining -= 1;
+    }
+    alert('The number that I was thinking of was ' + guessANumberAnswers[0]);
+    updateScore(numCorrect, + guessANumberQuestions.length + 1);
   }
-}
+};
 
 
