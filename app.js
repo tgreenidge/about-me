33d1987659bb29fe86f001d9ae34cc213ab092fe
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2019-03-28 09:44:21 -0700

    add question 7

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ var startQuiz = function(event){
 
     //set up questions, answers, and explanations for yes/no section of quiz
     var yesAndNoQuestions = [
-      'I started coding in college.', 
+      'I started coding in college.',
       'I served as an Electrician in the Navy.',
       'I recently learned the php programming language.',
       'My favorite color is blue',
@@ -39,7 +39,7 @@ var startQuiz = function(event){
 
     var yesAndNoAnswers = ['no', 'yes', 'no', 'no', 'yes'];
 
-    var yesAndNoExplanations = ['I started to code in high school.', 
+    var yesAndNoExplanations = ['I started to code in high school.',
       'I served as an Electrician in the Navy in a group called the Seabees.',
       'php is actually the laguage that I am least interested in learning, but if I have to, I will.',
       'Once upon a time, my favorite color was blue, but it is now orange',
@@ -50,7 +50,7 @@ var startQuiz = function(event){
     var guessANumberQuestions = ['I am thinking of a number between 1 and 10, can you guess what it is?'];
     var guessANumberAnswers = [getARandomNumber(10)];
     var guessANumberExplanations = ['The number that I was thinking about was '];
-  
+
     //Generic responses for correct/incorrect
     var correctResponseMessage = 'Yes, you are correct! ';
     var incorrectResponseMessage = 'Sorry, that is not the correct response. ';
@@ -61,12 +61,12 @@ var startQuiz = function(event){
 
     //Get array of elements for questions
     var questionDivs = document.getElementsByClassName('question');
-    
+
     //populate questions on DOM for Yes and No questions
     for (var i = 0; i < yesAndNoQuestions.length; i++) {
       // set up imput to match input to question index in array
       var idName = 'quiz-response-' + i;
-      questionDivs[i].innerHTML = '<h3>' + yesAndNoQuestions[i] + '</h3><input type="text" class="quiz-response" id=' + idName + ' placeholder="type yes/no"></input>'; 
+      questionDivs[i].innerHTML = '<h3>' + yesAndNoQuestions[i] + '</h3><input type="text" class="quiz-response" id=' + idName + ' placeholder="type yes/no"></input>';
     }
 
     updateScore(numCorrect, yesAndNoQuestions.length + guessANumberQuestions.length + 1);
@@ -76,7 +76,7 @@ var startQuiz = function(event){
     for (var j = 0; j < quizResponses.length; j++) {
       quizResponses[j].addEventListener('keypress', function (event){
         var keyPressed = event.which || event.keyCode;
-        
+
         //check to see if "Enter key is depressed"
         if (keyPressed === 13) {
           //tests that were used to extract value
@@ -88,7 +88,7 @@ var startQuiz = function(event){
           var userResponse = event.srcElement.value.toLowerCase().trim();
           var questionNumber = parseInt(this.id[this.id.length - 1]);
           console.log('Response entered for question ' + (questionNumber + 1));
-       
+
           if (userResponse === 'yes' || userResponse === 'no') {
             console.log('Valid response entered. User answered ' + userResponse + ' for question ' + (questionNumber + 1));
             console.log('input box is disabled');
@@ -105,7 +105,7 @@ var startQuiz = function(event){
 
               //display message for notifiction of correct response
               questionDivs[questionNumber].innerHTML += '    ' + correctResponseIcon + '<span class="correct">' + correctResponseMessage + yesAndNoExplanations[questionNumber] + '</span>';
-              
+
               //update current score and append to DOM
               numCorrect += 1;
               updateScore(numCorrect, yesAndNoQuestions.length + guessANumberQuestions.length + 1);
@@ -114,7 +114,7 @@ var startQuiz = function(event){
               console.log('j = ' + j);
               console.log('incorrect response entered');
               console.log(incorrectResponseMessage + yesAndNoExplanations[questionNumber]);
-              
+
               //display message for notification of incorrect response
               questionDivs[questionNumber].innerHTML += '    ' + incorrectResponseIcon + '<span class="incorrect">' + incorrectResponseMessage + yesAndNoExplanations[questionNumber] + '</span>';
             }
@@ -128,13 +128,13 @@ var startQuiz = function(event){
       });
     }
     //TODO refactor below a function for guessing game
-    var userGuess = prompt('You have 3 guesses. ' + guessANumberQuestions[0]);
+    var userGuess = prompt('Hello ' + userName + '. You have 3 guesses. ' + guessANumberQuestions[0]);
 
     while (isNaN(parseInt(userGuess))) {
       userGuess = prompt('Please enter a NUMBER. ' + guessANumberQuestions[0]);
     }
 
-    var numGuessesRemaining = 2;
+    var numGuessesRemaining = 3;
 
     while (numGuessesRemaining > 0 ) {
       var answer = guessANumberAnswers[0];
@@ -149,8 +149,29 @@ var startQuiz = function(event){
       }
       numGuessesRemaining -= 1;
     }
-    alert('The number that I was thinking of was ' + guessANumberAnswers[0]);
-    updateScore(numCorrect, + guessANumberQuestions.length + 1);
+    alert(guessANumberExplanations[0] + answer);
+    updateScore(numCorrect, + yesAndNoQuestions.length + guessANumberQuestions.length + 1);
+
+    //TODO refactor below to include on form
+    var placesLivedNames = ['MISSISSIPPI', 'RHODE ISLAND','TEXAS'];
+    var placesLived2Chars = ['MI', 'RI', 'TX'];
+    var lastQuestion = 'Hello ' + userName + '. You have 6 guesses. ' + 'Can you guess a state that I lived in other than New York and Washington?';
+    var userGuessForPlace = prompt(lastQuestion).toUpperCase();
+
+    numGuessesRemaining = 5;
+
+    while (numGuessesRemaining > 0 ) {
+      if (placesLivedNames.includes(userGuessForPlace) || placesLived2Chars.includes(userGuessForPlace)){
+        alert (correctResponseMessage);
+        numCorrect += 1;
+        break;
+      } else {
+        userGuessForPlace = prompt(lastQuestion).toUpperCase();
+      }
+      numGuessesRemaining -= 1;
+    }
+    alert('Other states that I lived are ' + placesLivedNames.join(', ') +'.');
+    updateScore(numCorrect, + yesAndNoQuestions.length + guessANumberQuestions.length + 1);
   }
 };
 
